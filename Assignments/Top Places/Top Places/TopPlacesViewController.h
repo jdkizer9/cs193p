acaae963003c971cccae0d69eb8fad8ba6af0859
James <james.kizer@gmail.com>
2014-02-25 14:51:14 -0500

    What are these changes?

---
+++ Assignments/Top Places/Top Places/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  Top Places
+//
+//  Created by James Kizer on 1/22/14.
+//  Copyright (c) 2014 JimmyTime Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UIViewController
+
+@end
---
+++ Assignments/Top Places/Top Places/TopPlacesViewController.m
@@ -0,0 +1,29 @@
+//
+//  TopPlacesViewController.m
+//  Top Places
+//
+//  Created by James Kizer on 1/22/14.
+//  Copyright (c) 2014 JimmyTime Software. All rights reserved.
+//
+
+#import "TopPlacesViewController.h"
+
+@interface TopPlacesViewController ()
+
+@end
+
+@implementation TopPlacesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
